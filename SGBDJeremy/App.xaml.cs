d1c18afeb9b35db3ecee48912442361f3cec7638
Tyler Daerden <daerdentyler@gmail.com>
2025-02-10 17:17:30 +0100

    Ajoutez des fichiers projet.

---
+++ SGBDJeremy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SGBDJeremy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
