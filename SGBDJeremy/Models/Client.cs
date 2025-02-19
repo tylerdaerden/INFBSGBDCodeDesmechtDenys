a4374f8f36c326bbfba2d04c70b806a62625b414
Tyler Daerden <daerdentyler@gmail.com>
2025-02-19 15:06:23 +0100

    Client Methods implemented

--- SGBDJeremy/Models/Client.cs
+++ SGBDJeremy/Models/Client.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static Java.Util.Jar.Attributes;
 
 namespace SGBDJeremy.Models
 {
@@ -52,26 +54,88 @@ namespace SGBDJeremy.Models
 		{ 
 			throw new NotImplementedException(); 
 		}
-		
-		public void EmailValidation(string mail)
+
+        /// <summary>
+        /// Validates an email address with the following criteria:
+        /// - Accepts upper and lower case characters, digits, and special characters "._%+-".
+        /// - Maximum 64 characters for the local part (before @).
+        /// - Maximum 63 characters for the domain part (after @).
+        /// - TLD between 2 and 10 characters, supporting modern and professional domains (e.g., @agency, @studio).
+        /// - Case-insensitive validation.
+        /// Returns true if the email is valid; otherwise, false.
+        /// </summary>
+        /// <param name="mailtocheck">The email address to validate.</param>
+        /// <returns>B>Boolean true if valid otherwise returns false</returns>
+        public static bool EmailValidation(string mailtocheck)
+        {
+            if (!string.IsNullOrEmpty(mailtocheck) && !string.IsNullOrWhiteSpace(mailtocheck))
+            {
+                if (!Regex.IsMatch(mailtocheck, @"^[a-z0-9._%+-]{1,64}@[a-z0-9-]{1,63}\.[a-z]{2,10}$", RegexOptions.IgnoreCase))
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+		/// <summary>
+		/// Validate a Belgian Phone Number based on +32 / 0032 indicators or 04 folowed by 8 numbers
+		/// returns true if phoneNumber is valid ; otherwise false.
+		/// </summary>
+		/// <param name="phonenumber"> the phone number to validate</param>
+		/// <returns>Boolean true if valid otherwise returns false</returns>
+        public static bool PhoneValidation(string phonenumber)
 		{
-			throw new NotImplementedException();
+			if(!string.IsNullOrEmpty(phonenumber) && !string.IsNullOrWhiteSpace(phonenumber))
+			{
+				//Denys note : i adapted to match with "/" or "." separators
+				if(!Regex.IsMatch(phonenumber, @"^(?:\+32|0032|0)4\d{2}[\s./]?\d{2}[\s./]?\d{2}[\s./]?\d{2}$"))
+				{
+					return false;
+				}
+				return true ;
+			}
+			return false;
 		}
 
-		public void PhoneValidation(string phonenumber)
-		{ 
-			throw new NotImplementedException(); 
-		}
+        /// <summary>
+		/// Validate a FamilyName : add of optionnal group in regex so it can accept composed name (ex Name-Name2 , present in certains culture or in married couples)
+		/// Accept a white Space between some name char (case of Name with space like "Particule Name"
+		/// Add of latin char (in case of Name containing latin char like "é" , "è" , "û" , "ö" etc ...
+		/// </summary>
+		/// <param name="name">the name to validate</param>
+		/// <returns>>Boolean true if valid otherwise returns false</returns>
+		public static bool NameValidation(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                if (Regex.IsMatch(name, @"^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[-' ][A-Za-zÀ-ÖØ-öø-ÿ]+)*$"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-		public void NameValidation(string name)
-		{ 
-			throw new NotImplementedException();
-		}
-
-		public void SurnameValidation(string surname)
+        /// <summary>
+        /// Validate a SurName : add of optionnal group in regex so it can accept composed name (ex SurName-SurName2)
+        /// Accept a white Space between some name char (case of Name with space like "Surname1 Surname2"
+        /// Add of latin char (in case of Name containing latin char like "é" , "è" , "û" , "ö" etc ...
+        /// </summary>
+        /// <param name="surname"></param>
+        /// <returns>Boolean true if valid otherwise returns false</returns>
+        public static bool SurnameValidation(string surname)
 		{
-			throw new NotImplementedException();
-		}
+            if (!string.IsNullOrWhiteSpace(surname))
+            {
+                if (Regex.IsMatch(surname, @"^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[-' ][A-Za-zÀ-ÖØ-öø-ÿ]+)*$"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
 
 
